3237d1bc8a12d655f16d0d9e234a2b3be55aed23
unknown <hamoudamdemagh@gmail.com>
2020-10-30 04:17:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserList from "./Components/UserList";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Navb from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navb />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route ecact path="/users" component={UserList} />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome</h1>
+      <img
+        src="https://miro.medium.com/max/3840/1*I_p_JFApwT8N-JLoKm6L0Q.jpeg"
+        className="users"
+        alt="conference icon"
+        width="1350"
+        height="800"
+      ></img>
+    </div>
+  );
+};
+export default Home;
